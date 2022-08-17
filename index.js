861c1a6ce911485c99ec67664dba61e11c8b1069
S Rabi <rabi.sanaye@yahoo.com>
2022-08-17 16:30:27 +0200

    c

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 //Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
-//1
 
-function filterRange(arr, a, b) {
-    //write your code here
-  }
-  
-  let arr = [5, 3, 8, 1];
-  
-  let filtered = filterRange(arr, 1, 4);
-  
-  console.log( filtered ); // 3,1 (matching values)
-  
-  console.log( arr ); // 5,3,8,1 (not modified)
+// 1:
+let numbers = [5, 3, 8, 1];
+let numTo4 = numbers.filter(function (number) {
+  return number < 4;
+});
+
+console.log(numTo4);
+console.log(numbers);
+
+console.log("-------------------------------------------");
 
 //Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
 //2
-  function filterRangeInPlace(arr, a, b) {
-    //write your code here
+function filterRangeInPlace(arr, a, b) {
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i];
+    if (val < a || val > b) {
+      arr.splice(i, 1);
+      i--;
+    }
   }
-  
-  let array = [5, 3, 8, 1];
-  
-  filterRangeInPlace(array, 1, 4); // removed the numbers except from 1 to 4
-  
-  console.log( array ); // [3, 1]
+}
+
+let array = [5, 3, 8, 1];
+
+filterRangeInPlace(array, 1, 4);
+
+console.log(array);
